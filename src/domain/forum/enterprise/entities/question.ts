602accba7c9c7e433b5b3e2b75a569a19c9ccb7d
Eduardo Tavares <notavyt@gmail.com>
2024-10-08 17:07:39 -0300

    factories de testes

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -5,7 +5,7 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-interface QuestionProps {
+export interface QuestionProps {
   authorId: UniqueEntityID
   bestAnswerId?: UniqueEntityID
   title: string
