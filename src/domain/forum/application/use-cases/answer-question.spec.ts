b8b195ef6ccadd959b0f02fe72aad41308027856
Eduardo Tavares <notavyt@gmail.com>
2024-10-08 16:53:21 -0300

    refatorando os testes unitarios

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,19 +1,24 @@
-import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+
 import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
-import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
-const fakeAnswersRepository: AnswersRepository = {
-  create: async (answer: Answer) => {},
-}
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: AnswerQuestionUseCase
 
-test('create an answer', async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
-
-  const answer = await answerQuestion.execute({
-    questionId: '1',
-    instructorId: '1',
-    content: 'Nova resposta',
+describe('Create Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new AnswerQuestionUseCase(inMemoryAnswersRepository)
   })
 
-  expect(answer.content).toEqual('Nova resposta')
+  it('should be able to create a answer', async () => {
+    const { answer } = await sut.execute({
+      questionId: '1',
+      instructorId: '1',
+      content: 'Nova resposta',
+    })
+
+    expect(answer.id).toBeTruthy()
+    expect(inMemoryAnswersRepository.items[0].id).toEqual(answer.id)
+  })
 })
